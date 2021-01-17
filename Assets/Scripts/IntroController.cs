b3bd2023dcb0a3e0b33b01af6fe8c1017462a0b1
joe2275 <whgmlwo1000@gmail.com>
2021-01-17 11:02:40 +0900

    [상세내용] 1. Unity 프로젝트 파일 추가 2. Intro 씬 추가 3. Main 씬 구현중

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroController : MonoBehaviour
+{
+    public void Event_EndOfIntro()
+    {
+        SceneManager.LoadScene("Scene_Main");
+    }
+}
